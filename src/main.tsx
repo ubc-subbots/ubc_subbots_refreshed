67aee9192bc0710ab58f8cef41f0c3c6a71b99ee
Griffin0-0 <griffin.ng.butchko@gmail.com>
2026-06-19 21:45:24 -0600

    Fix deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import { HashRouter } from "react-router-dom";
 import ScrollToTop from './ScrollToTop.tsx';
 
