8d2fdb4bf889ef3391b84bff6e9cdfe86f9f1dbe
Dorson Tang <apersonnameddorson@gmail.com>
2026-07-19 13:59:37 -0700

    some vision

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,6 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowUpRightFromSquare, faEnvelope, faLocationDot } from '@fortawesome/free-solid-svg-icons';
-import SubbotsLogo from '../../assets/icons/subbots.svg?react';
+import { faEnvelope, faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
 import './Contact.css';
 
@@ -72,33 +71,6 @@ function Contact() {
                     </div>
 
                 </div>
-            </section>
-
-            <section className="sponsor-cta">
-                <div className="sponsor-cta-inner">
-
-                    <SubbotsLogo className="background" />
-
-                    <h2>Sponsor Us</h2>
-
-                    <div className="divider" />
-
-                    <div className="content">
-                        <a href="https://donate.give.ubc.ca/page/80953/donate/1?transaction.dirgift=UBC%20Subbots%20Design%20team%20G2561" target="_blank" rel="noopener noreferror" className="button-primary">
-                            Donate
-                            <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
-                        </a>
-
-                        <p className="content-text-large">
-                            Your contributions directly support design teams in BC.
-                            Support next generation engineers and computer scientists specializing in
-                            mechanical, electrical, software, and robotics. Invest in the future of
-                            Vancouver, making tomorrow more advanced than today.
-                        </p>
-                    </div>
-
-                </div>
-
             </section>
 
         </section>
--- src/pages/Sponsors/Sponsors.tsx
+++ src/pages/Sponsors/Sponsors.tsx
@@ -1,5 +1,9 @@
 import SponsorCatalog from '../../components/SponsorCatalog/SponsorCatalog.tsx';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import SubbotsLogo from '../../assets/icons/subbots.svg?react';
+
 import './Sponsors.css';
 
 
@@ -35,6 +39,33 @@ function Sponsors() {
                 <SponsorTiers />
 
             </section> */}
+
+            <section className="sponsor-cta">
+                <div className="sponsor-cta-inner">
+
+                    <SubbotsLogo className="background" />
+
+                    <h2>Sponsor Us</h2>
+
+                    <div className="divider" />
+
+                    <div className="content">
+                        <a href="https://donate.give.ubc.ca/page/80953/donate/1?transaction.dirgift=UBC%20Subbots%20Design%20team%20G2561" target="_blank" rel="noopener noreferror" className="button-primary">
+                            Donate
+                            <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+                        </a>
+
+                        <p className="content-text-large">
+                            Your contributions directly support design teams in BC.
+                            Support next generation engineers and computer scientists specializing in
+                            mechanical, electrical, software, and robotics. Invest in the future of
+                            Vancouver, making tomorrow more advanced than today.
+                        </p>
+                    </div>
+
+                </div>
+
+            </section>
 
         </section>
     );
